67e0d119b7a7273a77023bb0d9792ed8f49a8a29
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2022-10-03 19:47:36 +0500

    updated the code and added comments

--- scope variables/main.js
+++ scope variables/main.js
@@ -1,9 +1,12 @@
+// creating variables of names and numbers respectively
 var vNum=100;
 var vName='Ali';
 var vNum2=30;
 var vName2='Murtaza bozdar';
+var vName3 = 'Shahzain Ahmed';
+var vNum3 = 86;
 
-
+// creating a new function
 function myFunc1()
 {
    var VText=document.getElementById('ChangeText').value;
@@ -13,11 +16,11 @@ document.getElementById('paragrapgh').innerHTML=VText;
 
 }
 
+// creating a new function again
 function myFunc2(){
 
-
-document.getElementById('paragrapgh').innerHTML=vName;
-document.getElementById('paragrapgh').innerHTML=vNum2;
-
+// printing the statements
+document.getElementById('paragraph').innerHTML=vName;
+document.getElementById('paragraph').innerHTML=vNum2;
 
 }
