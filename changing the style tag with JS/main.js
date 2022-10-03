6247343addc25af8711441dbab05c87ba0438a15
Raza Rehman <raza.rehman1997@gmail.com>
2022-10-03 11:27:03 +0500

    adding a new function

--- changing the style tag with JS/main.js
+++ changing the style tag with JS/main.js
@@ -1,10 +1,20 @@
 var num=4;
 
 
-function myFunc1()
+function Tagfuc()
 {
    document.getElementById('paragrapgh').style.color='red';
   
-   var VText=document.getElementById('ChangeText').value;
-   document.getElementById('paragrapgh').innerHTML=VText;
+   var Text1=document.getElementById('ChangeText').value;
+   document.getElementById('paragrapgh').innerHTML=Text1;
+   
+   
+      var Text2=document.getElementById('ChangeText').value;
+   document.getElementById('paragrapgh').innerHTML=Text2;
+
+  
+   
+      
+      var Text3=document.getElementById('ChangeText').value;
+   document.getElementById('paragrapgh').innerHTML=Text3;
    }
