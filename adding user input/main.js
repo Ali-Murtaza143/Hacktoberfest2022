cd402a8e0eeb339fe5f7a6a8f7fae107bf1c2199
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2022-10-03 19:49:41 +0500

    added new variables and comments

--- adding user input/main.js
+++ adding user input/main.js
@@ -1,15 +1,17 @@
+// creating a variable of names and numbers
 var Num=100;
 var Name='Ali';
 var Num2=200;
 var Name2='Murtaza';
+var Name3 = 'Shahzain Ahmed'; 
+var Num3 = 86;
 
-
+// creating a new function
 function myFunc1()
 {
    var Text=document.getElementById('ChangeText').value;
 
+// printing
 document.getElementById('paragrapgh').innerHTML=Text;
 
-
 }
-
