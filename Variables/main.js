11b2d181036addf874a096b11b0eaccd4a7fb22d
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2022-10-11 16:31:58 +0500

    added new contributor

--- Variables/main.js
+++ Variables/main.js
@@ -2,6 +2,7 @@
 var num1=100;
 var name1='Ali';
 var name2 = 'Shahzain Ahmed'; // new contributor 
+var name3 = 'Shahzain Ahmed from MUET'; // adding a new variable for a new contributor
 
 
 // creating a new addition function
