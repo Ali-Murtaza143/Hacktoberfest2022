1a80bb3127deed9f334c6093b70afb3c6429ff1b
Abhishek Krishna <akdpsgaya792@gmail.com>
2022-10-14 19:19:56 +0530

    Create Even_Odd.js

---
+++ Even_Odd.js
@@ -0,0 +1,14 @@
+//github - akrishna5
+
+//take input from the user
+const number = prompt("Enter a number: ");
+
+//check if the input number is EVEN
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the input number is ODD
+else {
+    console.log("The number is odd.");
+}
