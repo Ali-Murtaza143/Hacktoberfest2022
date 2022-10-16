440ac8a8c267302387164d9aeaac598c7c4c5880
Chetan Bogali <84176348+chetanbogali@users.noreply.github.com>
2022-10-16 15:39:24 +0530

    Create Average-of_three-numbers.py

---
+++ Average-of_three-numbers.py
@@ -0,0 +1,5 @@
+number1 = int(input("Please first number:"))
+number2 = int(input("Please second number:"))
+number3 = int(input("Please third number:"))
+result = (number1 + number2 + number3) / 3
+print("The average of three numbers: " , result)
