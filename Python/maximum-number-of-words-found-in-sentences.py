35becde12598ba57299f5d9ed2caee7159c86705
Av-Ku <65540110+Av-Ku@users.noreply.github.com>
2022-10-24 16:52:42 +0530

    Create maximum-number-of-words-found-in-sentences.py

---
+++ Python/maximum-number-of-words-found-in-sentences.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+
+        count1=[]
+        for i in range(0,len(sentences)):
+            a=sentences[i].split()         
+            b=len(a)
+            count1.append(b)
+            # count1.pop(0)
+            i=i+1
+            c=max(count1)
+        # print(count1)
+        # print(max(count1))
+        
+        return c
